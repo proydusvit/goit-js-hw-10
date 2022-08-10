b738fe4a7e2b4699bd5d92d2071bdd2f0d770477
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-08-10 22:08:18 +0300

    g

--- src/card.js
+++ src/card.js
@@ -8,15 +8,14 @@ export function cardCountriesList({ flags, name }) {
 }
 export function countryСard({ flags, name, capital, population, languages }) {
   return `
-      <div class="country-info__wrapper">
+     
         <img class="country-info__flags" src="${flags.svg}" alt="${name.official}" width="50" />
         <h2 class="country-info__name">${name.official}</h2>
       </div>
-      <p class="country-info__capital"><span class="country-info__weight">Capital:</span> ${capital}</p>
-      <p class="country-info__population"><span class="country-info__weight">Population:</span> ${population}</p>
-      <p class="country-info__languages"><span class="country-info__weight">Languages:</span> ${Object.values(
+      <p><span class="country-info__weight">Capital:</span> ${capital}</p>
+      <p><span class="country-info__weight">Population:</span> ${population}</p>
+      <p><span class="country-info__weight">Languages:</span> ${Object.values(
         languages,
       )}</p>
-    </div>
   `;
 }
